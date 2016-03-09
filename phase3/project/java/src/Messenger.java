6be543e3596de686e9cd12b1eee7dc32df79a912
shilparoys <shilparoys@yahoo.com>
2016-03-09 01:33:06 -0800

    add chat member?

--- phase3/project/java/src/Messenger.java
+++ phase3/project/java/src/Messenger.java
@@ -553,7 +553,7 @@ public class Messenger {
             switch (readChoice()){
                 case 1: createChat(esql, authorizedUser); break;
                 case 2: chatViewer(esql, authorizedUser); break;
-                case 3: addChatMember(esql, authorizedUser); break;
+                case 3: addChatMember(esql, authorizedUser, "5"); break;
                 case 4: removeChatMember(esql, authorizedUser); break;
                 case 5: deleteChat(esql, authorizedUser); break;
                 case 6: chatmenu = false; break;
@@ -596,6 +596,7 @@ public class Messenger {
 
             for(int i = 0; i < chatUsers.size(); i++){
                 query = String.format("INSERT INTO CHAT_LIST(chat_id, member) VALUES('%s', '%s')", chatId, authorizedUser);
+                esql.executeUpdate(query);
             }
             String print = "New chat has been created with " + authorizedUser + " ";
             for(int i = 0; i < chatUsers.size(); i++){
@@ -611,7 +612,38 @@ public class Messenger {
    public static void chatViewer(Messenger esql, String authorizedUser){
    }
 
-   public static void addChatMember(Messenger esql, String authorizedUser){
+   public static void addChatMember(Messenger esql, String authorizedUser, String chatId){
+
+        try{
+             System.out.println("Enter user name to add to chat");
+            String chatName = in.readLine();
+             if(chatName == ""){
+                System.out.println("Cannot enter blank user id");
+                return;
+            }
+
+            //checking if it is initial sender
+            String query = String.format("SELECT * FROM CHAT WHERE init_sender = '%s' AND chat_id = '%s'", authorizedUser, chatId);
+            int userNum = esql.executeQuery(query);
+            if(userNum <= 0){
+                System.out.println("Not initial sender");
+                return;
+            }
+
+            query = String.format("SELECT * FROM USR WHERE login = '%s'", chatName);
+            userNum = esql.executeQuery(query);
+            if(userNum <= 0){
+                System.out.println("Given user id does not exisit");
+                return;
+            }
+            query = String.format("INSERT INTO CHAT_LIST(chat_id, member) VALUES('%s', '%s')", chatId, authorizedUser);
+            esql.executeUpdate(query);
+            String print = chatName + " is added to the chat";
+            System.out.println(print);
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
    }
 
    public static void removeChatMember(Messenger esql, String authorizedUser){
